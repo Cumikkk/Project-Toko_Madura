7c539b408480824becd08f7acaf28bece03ca4ef
Cumikkk. <m.fahrulalfanani25@gmail.com>
2026-07-21 14:08:31 +0700

    Fix DataTable Requested unknown parameter 5 by aligning admin tabledata columns

--- admin/ajax/tabledata/admin/view.php
+++ admin/ajax/tabledata/admin/view.php
@@ -1,17 +1,25 @@
 <?php
 $dt->query("
     SELECT
+        NOW() as ADM_TIMESTAMP,
         username as ADM_USER,
         nama_lengkap as ADM_NAME,
-        role as ADMROLE_NAME,
-        1 as ADM_STS,
+        'Master' as ADMROLE_NAME,
+        1 as ADM_LEVEL,
+        -1 as ADM_STS,
+        password as ADM_PASS,
         id_users as ID_ADM,
-        id_users as ADM_ID
+        id_users as ADM_ID,
+        'Indonesia' as COUNTRY_NAME
     FROM users
     WHERE role = 'master'
 ");
 
 $dt->hide('ID_ADM');
+$dt->hide('ADM_PASS');
+$dt->hide('ADM_LEVEL');
+$dt->hide('COUNTRY_NAME');
+
 $dt->edit('ADM_STS', function($data) {
     return "<span class='badge bg-success'>Active</span>";
 });
